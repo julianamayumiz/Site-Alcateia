5cb9765409d41f2833da4bb39938589d73bf9299
julianamayumiz <juliana.mayumi14@gmail.com>
2026-04-16 17:44:56 -0300

    ajuste botoes de pontuacal

--- src/pages/sw.js
+++ src/pages/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'alcateia-chefia-v1';
+const CACHE_VERSION = 'alcateia-chefia-v2';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
