d07fd949536fb60ef2774741a72e1cd40781a1cd
julianamayumiz <juliana.mayumi14@gmail.com>
2026-04-16 17:51:37 -0300

    ajuste foto do app

--- src/pages/sw.js
+++ src/pages/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'alcateia-chefia-v2';
+const CACHE_VERSION = 'alcateia-chefia-v3';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
